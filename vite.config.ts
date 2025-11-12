0c20d0fbd34655f046614a6825e71a76ed4c5188
Vitalii <witalik.karwack@gmail.com>
2025-11-11 20:48:16 -0800

    initi

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import path from 'node:path'
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/',
   resolve: { alias: { '@': path.resolve(__dirname, './src') } }
 })
